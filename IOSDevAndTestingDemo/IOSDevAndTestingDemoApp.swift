7c6100780b732b5b217bd2e353e5d7466944287f
Hanendra Singh <hanendra.pratap.singh@walmart.com>
2021-12-26 17:14:50 +0530

    Initial Commit

---
+++ IOSDevAndTestingDemo/IOSDevAndTestingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSDevAndTestingDemoApp.swift
+//  IOSDevAndTestingDemo
+//
+//  Created by Hanendra Pratap Singh on 26/12/21.
+//
+
+import SwiftUI
+
+@main
+struct IOSDevAndTestingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
